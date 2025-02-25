04a2e8c9167774b7dd549e0831fe16650464af3e
EugeneBlogs <108799072+EugeneBlogs@users.noreply.github.com>
2025-02-25 15:39:56 +0200

    Update Help.py

--- Prototype 16/Help.py
+++ Prototype 16/Help.py
@@ -13,9 +13,14 @@ from functools import lru_cache
 @lru_cache(None)
 # ВАШ КОД
 def F(n):
-    if n <= 1:
-        return n
-    if n > 1 and n % 2 == 0:
-        return F(n // 4 - 1) + 10
-    if n > 1 and n % 2 != 0:
-        return n + F(n + 1)
+    if n < 11:
+        return 10
+    else:
+        return n + F(n - 1)
+
+
+# Чтобы данные закэшировались, вызываем функцию примерно столько раз, чтобы все используемые числа были в диапозоне
+for i in range(1, 2025):
+    F(i)
+
+print(F(2022) - F(2019))  # Вызываем функцию уже с нужным условием
