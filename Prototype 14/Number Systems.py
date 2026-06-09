a14fafd37a6fc54419847722f41f76b2e64f2216
EugeneBlogs <108799072+EugeneBlogs@users.noreply.github.com>
2026-06-09 12:34:57 +0200

    Update Number Systems.py

--- Prototype 14/Number Systems.py
+++ Prototype 14/Number Systems.py
@@ -12,8 +12,28 @@ while number > 0:
 result = result[::-1]
 print(result)
 
-# Перевод из "N" системы счисления в 10
+# Перевод из "N" системы счисления в 10 ("2 <= N <= 36")
 
 base = int(input("Система счисления: "))
 number = input("Число: ")
+print(int(number, base))
+
+# Перевод из "N" системы счисления в 10 (универсальный метод)
+
+from string import ascii_uppercase as al
+
+al = "0123456789" + al
+
+base = int(input("Система счисления: "))
+number = input("Число: ")
+
+
+def int(s, n):
+    s = s[::-1]
+    sm = 0
+    for i in range(len(s)):
+        sm += al.index(s[i]) * n ** i
+    return sm
+
+
 print(int(number, base))
